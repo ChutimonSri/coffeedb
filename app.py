6208ef83ba325acfd45dbd4c055df9c6c2051717
chutimonSri <chutimon.sro@student.mahidol.edu>
2023-07-11 12:34:18 +0700

    added navbar + able to link to another page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, request, url_for, redirect, render_template
 
 app = Flask(__name__)
 
@@ -6,6 +6,49 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        # do stuff when the form is submitted
+
+        # redirect to end the POST handling
+        # the redirect can be to the same route or somewhere else
+        return redirect(url_for('home'))
+
+    # show the form, it wasn't submitted
+    return render_template('home.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        return redirect(url_for('contact'))
+    return render_template('contact.html')
+
+@app.route('/cart', methods=['GET', 'POST'])
+def cart():
+    if request.method == 'POST':
+        return redirect(url_for('cart'))
+    return render_template('cart.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return redirect(url_for('login'))
+    return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        return redirect(url_for('signup'))
+    return render_template('signup.html')
+
+@app.route('/menu', methods=['GET', 'POST'])
+def menu():
+    if request.method == 'POST':
+        return redirect(url_for('menu'))
+    return render_template('menu.html')
+
+
+
 if __name__ == '__main__':
-	app.run(debug=True)
-	
+	app.run(debug=True);
