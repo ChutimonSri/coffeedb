79b775467c68e26d0de3bfbf004b41689512b03a
chutimonSri <chutimon.sro@student.mahidol.edu>
2023-07-13 17:24:48 +0700

    user management for admin is done

--- app.py
+++ app.py
@@ -54,18 +54,17 @@ def login():
         queryStatement = f"SELECT * FROM user WHERE email = '{email}'"
         numRow = cur.execute(queryStatement)
         if numRow > 0:
-            user =  cur.fetchone()
+            user = cur.fetchone()
             if check_password_hash(user['password'], loginForm['password']):
-
                 # Record session information
                 session['login'] = True
+                session['user_id'] = user['user_id']  # Store the user ID in the session
                 session['email'] = user['email']
                 session['userroleid'] = str(user['role_id'])
                 session['firstName'] = user['first_name']
                 session['lastName'] = user['last_name']
                 print(session['email'] + " roleid: " + session['userroleid'])
                 flash('Welcome ' + session['firstName'], 'success')
-                # flash("Log In successful",'success')
                 return redirect('/')
             else:
                 cur.close()
@@ -79,6 +78,7 @@ def login():
         return redirect('/')
     return render_template('login.html')
 
+
 @app.route('/signup/', methods=['GET', 'POST'])
 def signup():
     if request.method == 'GET':
@@ -87,7 +87,7 @@ def signup():
         userDetails = request.form
         # Check the password and confirm password
         if userDetails['password'] != userDetails['confirm_password']:
-            flash('Passwords do not match!','danger')
+            flash('Passwords do not match!', 'danger')
             return render_template('signup.html')
         p1 = userDetails['first_name']
         p2 = userDetails['last_name']
@@ -98,19 +98,34 @@ def signup():
 
         queryStatement = (
             f"INSERT INTO "
-            f"user(first_name,last_name, email, password, role_id) "
-            f"VALUES('{p1}', '{p2}', '{p3}','{hashed_pw}', 1)"
+            f"user(first_name, last_name, email, password, role_id) "
+            f"VALUES('{p1}', '{p2}', '{p3}', '{hashed_pw}', 1)"
         )
         print(check_password_hash(hashed_pw, p4))
         print(queryStatement)
         cur = mysql.connection.cursor()
         cur.execute(queryStatement)
         mysql.connection.commit()
+
+        # Retrieve the user ID from the database
+        queryStatement = f"SELECT user_id FROM user WHERE email = '{p3}'"
+        cur.execute(queryStatement)
+        user_id = cur.fetchone()['user_id']
         cur.close()
 
-        flash("Form Submitted Successfully.",'success')
-        return redirect('/')    
+        # Set session variables to automatically log in the user
+        session['login'] = True
+        session['user_id'] = user_id  # Store the user ID in the session
+        session['email'] = p3
+        session['userroleid'] = '1'
+        session['firstName'] = p1
+        session['lastName'] = p2
+
+        flash("Form Submitted Successfully.", 'success')
+        return redirect('/')
+
     return render_template('signup.html')
+
 
 @app.route('/menu', methods=['GET', 'POST'])
 def menu():
@@ -118,11 +133,120 @@ def menu():
         return redirect(url_for('menu'))
     return render_template('menu.html')
 
+
+@app.route('/user_mnm/')
+def user_mnm():
+    cur = mysql.connection.cursor()
+    queryStatement = f"SELECT * FROM user order by role_id"
+    result_value = cur.execute(queryStatement) 
+    if result_value > 0:
+        users = cur.fetchall()
+        return render_template('user_management.html', users=users)
+    else:
+        return render_template('user_management.html')
+
+
 @app.route('/logout')
 def logout():
     session.clear()
     flash("You have been logged out", 'info')
     return redirect('/')
+
+@app.route('/add_user/', methods=['GET', 'POST'])
+def add_user():
+    if request.method == 'GET':
+        return render_template('add_user.html')
+    elif request.method == 'POST':
+        userDetails = request.form
+        # Check the password and confirm password
+        if userDetails['password'] != userDetails['confirm_password']:
+            flash('Passwords do not match!','danger')
+            return render_template('add_user.html')
+        p1 = userDetails['first_name']
+        p2 = userDetails['last_name']
+        p3 = userDetails['email']
+        p4 = userDetails['password']
+        hashed_pw = generate_password_hash(p4)
+        print(p1 + "," + p2 + "," + p3 + "," + p4 + "," + hashed_pw)
+
+        if userDetails['role'] == 'admin':
+            user_id = 0
+        else:
+            user_id = 1
+
+        queryStatement = (
+            f"INSERT INTO "
+            f"user(first_name,last_name, email, password, role_id) "
+            f"VALUES('{p1}', '{p2}', '{p3}','{hashed_pw}', {user_id})"
+        )
+        print(check_password_hash(hashed_pw, p4))
+        print(queryStatement)
+        cur = mysql.connection.cursor()
+        cur.execute(queryStatement)
+        mysql.connection.commit()
+
+        flash("Form Submitted Successfully.", 'success')
+        return redirect('/user_mnm/')
+    return render_template('add_user.html')
+
+@app.route('/edit_user/<int:id>/', methods=['GET', 'POST'])
+def edit_user(id):
+    if request.method == 'POST':
+        cur = mysql.connection.cursor()
+        new_firstname= request.form['first_name']
+        new_lastname= request.form['last_name']
+
+        queryStatement = f"UPDATE user SET first_name= '{new_firstname}', last_name = '{new_lastname}' WHERE user_id = {id}"
+        print(queryStatement)
+        cur.execute(queryStatement)
+        mysql.connection.commit()
+        cur.close()
+
+        session['firstName'] = new_firstname
+        session['lastName'] = new_lastname
+
+        flash('User updated', 'success')
+        return redirect('/user_mnm/')
+    else:
+        cur = mysql.connection.cursor()
+        queryStatement = f"SELECT * FROM user WHERE user_id = {id}"
+        print(queryStatement)
+        result_value = cur.execute(queryStatement)
+        if result_value > 0:
+            member = cur.fetchone()
+            member_form = {}
+            member_form['first_name'] = member['first_name']
+            member_form['last_name'] = member['last_name']
+            return render_template('edit_user.html', member_form=member_form)
+    
+from flask import session
+
+@app.route('/delete_user/<int:id>/', methods=['GET'])
+def delete_user(id):
+
+    if request.method == 'GET':
+        if id == session.get('user_id'):
+            cur = mysql.connection.cursor()        
+            queryStatement = f"DELETE FROM user WHERE user_id = {id}"
+            print(queryStatement)
+            cur.execute(queryStatement)
+            mysql.connection.commit()
+            cur.close()
+            session.clear()  # Clear the session to log them out
+            flash('Deleted user successfully', 'success')
+            return redirect(url_for('login'))
+        else:
+            cur = mysql.connection.cursor()        
+            queryStatement = f"DELETE FROM user WHERE user_id = {id}"
+            print(queryStatement)
+            cur.execute(queryStatement)
+            mysql.connection.commit()
+            cur.close()
+            flash('Deleted user successfully', 'success')
+            return redirect('/user_mnm/')
+        
+
+    
 
 if __name__ == '__main__':
 	app.run(debug=True);
